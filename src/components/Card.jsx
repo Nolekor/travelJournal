2854cd182ad6aebb549677df62e4014c61c60b99
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-14 18:00:52 +0200

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Card = (props) => {
+  console.log(props);
+  return (
+    <div className="card-container">
+      <div className="img-container">
+        <img className="img-size" src={props.item.imageUrl} alt="" />
+      </div>
+      <div className="info-container">
+        <div className="loc-info">
+          <img className="loc-img" src="/src/images/location.com.png" alt="" />
+          <h4 className="location">{props.item.location}</h4>
+          <a href={props.item.googleMapsUrl}>View on Google Maps!</a>
+        </div>
+        <h3 className="card-title">{props.item.title}</h3>
+        <div className="card-date">
+          <p>-- From {props.item.startDate} --</p>
+          <p className="end"> To {props.item.endDate} --</p>
+        </div>
+        <p className="card-description">{props.item.description}</p>
+      </div>
+    </div>
+  );
+};
