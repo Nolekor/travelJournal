07fe4e9d6303d5465e04d02243e9730828aea69b
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-14 16:57:05 +0200

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="nav-container">
+      <img
+        className="nav-icon"
+        src="/src/images/pngwing.com.png"
+        alt="little world icon"
+      />
+      <h2 className="nav-title">My travel journal</h2>
+    </div>
+  );
+};
